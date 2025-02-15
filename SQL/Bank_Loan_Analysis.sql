35a8303c7b3bc6c07edbc249b5ef5d6d45eef74f
bibash12345 <bibashdahal2054@gmail.com>
2025-02-15 19:15:09 +0545

    Update Bank_Loan_Analysis.sql

--- SQL/Bank_Loan_Analysis.sql
+++ SQL/Bank_Loan_Analysis.sql
@@ -9,7 +9,7 @@ Select COUNT(id) as Month_To_Date_Loan_Applications from bank_loan_data
 Where MONTH(issue_date) = 12 and YEAR(issue_date) = 2021;
 
 -- Count loan applications for the previous month (November 2021)
- Select COUNT(id) as Previous_Month_To_Date_Loan_Applications from bank_loan_data 
+Select COUNT(id) as Previous_Month_To_Date_Loan_Applications from bank_loan_data 
 Where MONTH(issue_date) = 11 and YEAR(issue_date) = 2021;
 
 -- Calculate the total funded loan amount
@@ -99,9 +99,9 @@ WHERE loan_status = 'Charged Off'
 
 --4) FOR LOAN STATUS
 --Calculates the key loan metrics by loan status
-	SELECT
+    SELECT
         loan_status,
-        COUNT(id) AS Total_Loan_Applications,
+    COUNT(id) AS Total_Loan_Applications,
         SUM(total_payment) AS Total_Amount_Received,
         SUM(loan_amount) AS Total_Funded_Amount,
         AVG(int_rate * 100) AS Interest_Rate,
@@ -112,21 +112,13 @@ WHERE loan_status = 'Charged Off'
         loan_status
 
 --Calculates the key loan metrics by loan status For December 2021
-	SELECT 
+     SELECT 
 	loan_status, 
 	SUM(total_payment) AS MTD_Total_Amount_Received, 
 	SUM(loan_amount) AS MTD_Total_Funded_Amount 
-FROM bank_loan_data
-WHERE MONTH(issue_date) = 12 
-GROUP BY loan_status
-
-
-
-
-
-
-
-
+     FROM bank_loan_data
+     WHERE MONTH(issue_date) = 12 
+     GROUP BY loan_status
 
 
 
